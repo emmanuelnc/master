3bb22790b1588cf131f79311dbc1c7b28b7b45a6
emmanuelnc <30611717+emmanuelnc@users.noreply.github.com>
2017-08-10 15:39:33 -0500

    m

--- Notes/SQL/Tables.sql
+++ Notes/SQL/Tables.sql
@@ -1,3 +1,9 @@
 DBCC FREEPROCCACHE
 
 SET IDENTITY_INSERT FYC.Cat_Finanzas_Proveedores   ON
+
+alter table SC.TableCAtdrop constraint FK_Cat_Finanzas_Contactos_SC.TableCAt
+alter table FYC.SC.TableCAtDirectosdrop constraint FK_SC.TableCAtDirectos_SC.TableCAt
+
+alter table SC.TableCAtadd constraint FK_Cat_Finanzas_Contactos_SC.TableCAtforeign key( ProveedorID) references FYC.SC.TableCAt(ProveedorID) on DELETE CASCADE
+alter table FYC.SC.TableCAtDirectosadd constraint FK_SC.TableCAtDirectos_SC.TableCAtforeign key( ProveedorID) references FYC.SC.TableCAt(ProveedorID) on DELETE no action
