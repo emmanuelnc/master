473ae7a41d08c0f6d5b4646e3752de2b0d8b0806
emmanuelnc <30611717+emmanuelnc@users.noreply.github.com>
2017-08-31 14:53:04 -0500

    Update Tables.sql
    ddl definition

--- Notes/SQL/Tables.sql
+++ Notes/SQL/Tables.sql
@@ -7,3 +7,13 @@ alter table FYC.SC.TableCAtDirectosdrop constraint FK_SC.TableCAtDirectos_SC.Tab
 
 alter table SC.TableCAtadd constraint FK_Cat_Finanzas_Contactos_SC.TableCAtforeign key( ProveedorID) references FYC.SC.TableCAt(ProveedorID) on DELETE CASCADE
 alter table FYC.SC.TableCAtDirectosadd constraint FK_SC.TableCAtDirectos_SC.TableCAtforeign key( ProveedorID) references FYC.SC.TableCAt(ProveedorID) on DELETE no action
+
+--Rename column
+sp_rename 'dbo.old_table_name.DD', 'IDDocumento', 'COLUMN';
+
+--rename table 
+sp_rename 'old_table_name', 'new_table_name';
+
+--change shcema name
+ALTER SCHEMA TargetSchema 
+    TRANSFER SourceSchema.TableName;
