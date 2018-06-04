689fa8b5c39e40b1f9120f9201b11c22fa1d44c9
emmanuelnc <30611717+emmanuelnc@users.noreply.github.com>
2018-06-04 17:23:04 -0500

    Update LINQ TO XML.vb
    Create nodes from list

--- LINQ TO XML.vb
+++ LINQ TO XML.vb
@@ -36,3 +36,19 @@ Dim Solicitud As XDocument = XDocument.Parse(Res.d.Solicitud)
 
                             'Actualizamos los datos de la solicitud
                             Solicitud.SetRespuestaSolicitud(pFolio, True, doc.InnerXml.ToString)
+			
+			
+			
+    		Dim Solicitud As XDocument = _
+                        <?xml version="1.0" encoding="UTF-8"?>
+                        <PoolServicios>
+                            <Servicio ID="15" Descripcion=<%= Servicio %> Observacion=<%= txtObservaciones.Text %> Folio="" Descuento=<%= txtDescuento.Value %> Asesor=<%= Asesor %> TipoDescuento=<%= cmbTipoDescuento.SelectedItem.DisplayText %>>
+                                <Cliente ID=<%= TxtCliente1.Value %> Nombre=<%= lblNombreCliente.Text %> ReferenciaBancaria=<%= lblReferenciaBancaria.Text %> ClienteVendedorID=<%= lblClienteVendedorID.Text %>/>
+                                <Facturas>
+                                    <%= From f In listFacturas Where f.Incluir = True And f.Saldo > 0 Select <Factura NoDocumento=<%= f.NoDocumento %> Serie=<%= f.Serie %> Importe=<%= f.Importe %> Saldo=<%= f.Saldo %> MetodoPagoID=<%= f.MetodoPagoID %> FechaDocumento=<%= f.FechaDocumento %> FechaVencimiento=<%= f.FechaVencimiento %> Descuento=<%= f.Descuento %>/> %>
+                                </Facturas>
+                            </Servicio>
+                        </PoolServicios>
+
+				
+				
