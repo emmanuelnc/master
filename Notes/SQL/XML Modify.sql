711d1ad4ae2730231d1175e46d25f6d053785685
emmanuelnc <30611717+emmanuelnc@users.noreply.github.com>
2018-06-14 08:19:49 -0500

    Update XML Modify.sql

--- Notes/SQL/XML Modify.sql
+++ Notes/SQL/XML Modify.sql
@@ -12,3 +12,16 @@ UPDATE @HR_XML
 --SET Salaries.modify('insert attribute  TipoCorreo{"Principal"} into (/Correos/Correo)[1]')
 SET Salaries.modify('insert <TipoCorreo /> into (/Correos/Correo)[1]')
 select * from @HR_XML
+
+-- EDIT -- EDIT -- EDIT
+
+
+declare @correo as xml = 
+'<Correos><Correo><Direccion>cmexi@autopartesymas.mx</Direccion><FacturaElectronica>0</FacturaElectronica></Correo><Correo><Direccion>lrromero@pasa.mx</Direccion><FacturaElectronica>0</FacturaElectronica></Correo></Correos>
+'
+
+select 
+@correo.value('(Correos/Correo/Direccion/text())[1]','varchar (150)') ProvID
+set @correo.modify('delete //Correos//*[contains(.,"@autopartesymas.mx")]')
+--set @correo.modify('delete //Correos//*')
+select @correo
